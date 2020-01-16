aa17bfea98f8f95e46dbd6b2e72857080aaa90fb
Oarabile Macdonald <Oarabile.Macdonald@smollan.com>
2020-01-16 14:08:35 +0200

    msg23

--- api/jitandkaizalaInter.php
+++ api/jitandkaizalaInter.php
@@ -30,57 +30,55 @@ foreach (getallheaders() as $name => $value) {
 if (strlen($G_TOKEN) > 10)
 {
     //Validate the Basic Authorization
-
     //Check if Basic Time has no expired
-    
     //Testing
-    //$details = json_decode(file_get_contents("php://input"));
-    $fh = fopen('log.txt', 'w') or die("Can't open file.");
+    $details = json_decode(file_get_contents("php://input"));
+    //$fh = fopen('log.txt', 'w') or die("Can't open file.");
     // output the value as a variable by setting the 2nd parameter to true
-    //$results = print_r($details, true);
-    $results ="Tester";
-    fwrite($fh, $results);
-    fclose($fh);
+    $results = print_r($details, true);
+    //$results ="Tester";
+    //fwrite($fh, $results);
+    //fclose($fh);
 	
 	
 	// Instantiation and passing `true` enables exceptions
      $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = "macdonaldnkoana@gmail.com";                     // SMTP username
-    $mail->Password   = "macdOM27@m7";                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 465;                                    // TCP port to connect to
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = "macdonaldnkoana@gmail.com";                     // SMTP username
+        $mail->Password   = "macdOM27@m7";                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 465;                                    // TCP port to connect to
 
-    //Recipients
-    $mail->setFrom('macdonaldnkoana@gmail.com', 'testerGmail');
-   $mail->addAddress('oarabile.macdonald@smollan.com', 'testingOutlook');
-    /*$mail->addAddress('ellen@example.com');
-   $mail->addReplyTo('info@example.com', 'Information');
-   $mail->addCC('cc@example.com');
-   $mail->addBCC('bcc@example.com');*/
+        //Recipients
+        $mail->setFrom('macdonaldnkoana@gmail.com', 'testerGmail');
+       $mail->addAddress('oarabile.macdonald@smollan.com', 'testingOutlook');
+        /*$mail->addAddress('ellen@example.com');
+       $mail->addReplyTo('info@example.com', 'Information');
+       $mail->addCC('cc@example.com');
+       $mail->addBCC('bcc@example.com');*/
 
-    // Attachments
-    /*$mail->addAttachment('/var/tmp/file.tar.gz');         
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); */  
+        // Attachments
+        /*$mail->addAttachment('/var/tmp/file.tar.gz');
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); */
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b> '.$results;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    $mail->send();
-    echo 'Message has been sent';
-	///echo json_encode("The API EndPoint is working properly");
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+        $mail->send();
+        echo 'Message has been sent';
+        ///echo json_encode("The API EndPoint is working properly");
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }else{
     echo json_encode("Please provided Basic Authorization to proceed");
 }
