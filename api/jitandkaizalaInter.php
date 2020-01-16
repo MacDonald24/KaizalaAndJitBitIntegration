35bc720429185f8cbc4b16ec2104b2cbf56b8da8
Oarabile Macdonald <Oarabile.Macdonald@smollan.com>
2020-01-16 11:58:59 +0200

    msg36

--- api/jitandkaizalaInter.php
+++ api/jitandkaizalaInter.php
@@ -1,5 +1,8 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
@@ -9,8 +12,12 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 date_default_timezone_set('Africa/Johannesburg');
-require 'sendemail.php';
-$G_TAKE = "";
+
+
+
+require '../vendor/autoload.php';
+
+$G_TOKEN = "";
 foreach (getallheaders() as $name => $value) {
     if ($name == "Authorization") {
         $G_TOKEN = $value;
@@ -36,13 +43,44 @@ if (strlen($G_TOKEN) > 10)
     fclose($fh);
 	
 	
-	$ToEmailList = "oarabile.macdonald@smollan.com";
-	$Subject  = "Testing the world";
-	$bodyContent = "The API EndPoint is working properly";
+	// Instantiation and passing `true` enables exceptions
+     $mail = new PHPMailer(true);
 
-	SendEmail($ToEmailList,$Subject,$bodyContent);
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = "macdonaldnkoana@gmail.com";                     // SMTP username
+    $mail->Password   = "macdOM27@m7";                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('macdonaldnkoana@gmail.com', 'testerGmail');
+   $mail->addAddress('oarabile.macdonald@smollan.com', 'testingOutlook');
+    /*$mail->addAddress('ellen@example.com');
+   $mail->addReplyTo('info@example.com', 'Information');
+   $mail->addCC('cc@example.com');
+   $mail->addBCC('bcc@example.com');*/
+
+    // Attachments
+    /*$mail->addAttachment('/var/tmp/file.tar.gz');         
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); */  
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
 	///echo json_encode("The API EndPoint is working properly");
-    
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 }else{
     echo json_encode("Please provided Basic Authorization to proceed");
 }
